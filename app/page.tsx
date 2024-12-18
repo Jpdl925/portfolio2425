3b7aa6dfb22cbd7dc3e8a758ea7765dcffd127b1
Jpdl925 <jtpg925@gmail.com>
2024-12-17 20:54:33 -0800

    Added griditems, gradient, and pictures

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import Grid from "@/components/Grid";
+import { FloatingNavbar } from "@/components/ui/FloatNavbar";
 import Hero from "@/components/ui/Hero";
+import { FaHome } from "react-icons/fa";
 
 
 
@@ -8,7 +11,9 @@ export default function Home() {
    <main className="relative h-full bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
     <div className="max-w-7xl w-full ">
       <h1 className="text-white-200">Hello, Portfolio</h1>
+      <FloatingNavbar navItems={[{name:'Home',link:'/',icon:<FaHome/>}]}/>
       <Hero/>
+      <Grid/>
     </div>
    </main>
   );
--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -1,4 +1,10 @@
+import { BentoGrid } from "./BentoGrid";
+import { FloatingNavbar } from "./FloatNavbar";
 import { Spotlight } from "./Spotlight"
+import TailwindButton from "./TailwindButton"
+import { TextGenerateEffect } from "./TextGenerateEffect"
+import { GoProjectRoadmap } from "react-icons/go";
+
 
 const Hero = () => {
   return (
@@ -18,10 +24,24 @@ const Hero = () => {
             fill="blue"
             />
         </div>
-        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.06] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
           {/* Radial gradient for the container to give a faded look */}
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         </div>
+
+        <div className="flex justify-center relative my-20 z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center">
+              <h2 className="uppercase tracking-widest text-xs text-center text-gray-50 max-w-80">Dynamic web Magic Next.js</h2>
+
+              <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Hey There! I'm your friendly neighborhood coder"/>
+              <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hello, I am Julius</p>
+              <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">A Next.js Developer based in Stockton, CA</p>
+              <a href="#about"><TailwindButton position="right" title="My Projects" icon={<GoProjectRoadmap/>}/></a>
+          </div>
+        </div>
+
+
+
     </div>
     </>
   )
