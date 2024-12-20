1c67f1b64ad28cdfe6ed72230a42c9592db3640a
Jpdl925 <jtpg925@gmail.com>
2024-12-19 20:27:10 -0800

    Putting back original deploy from @/data

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNavbar } from "@/components/ui/FloatNavbar";
 import Hero from "@/components/ui/Hero";
-import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
 
 
 
@@ -12,7 +12,7 @@ export default function Home() {
    <main className="relative h-full bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
     <div className="max-w-7xl w-full ">
       <h1 className="text-white-200">Hello, Portfolio</h1>
-      <FloatingNavbar navItems={[{name:'Home',link:'/',icon:<FaHome/>}]}/>
+      <FloatingNavbar navItems={navItems}/>
       <Hero/>
       <Grid/>
       <RecentProjects/>
