ae0368618727d69da24076ceec29ed06fe506f10
Jpdl925 <jtpg925@gmail.com>
2024-12-19 20:26:15 -0800

    Test redeploy

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNavbar } from "@/components/ui/FloatNavbar";
 import Hero from "@/components/ui/Hero";
-import { navItems } from "@/data/index";
+import { FaHome } from "react-icons/fa";
 
 
 
@@ -12,7 +12,7 @@ export default function Home() {
    <main className="relative h-full bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
     <div className="max-w-7xl w-full ">
       <h1 className="text-white-200">Hello, Portfolio</h1>
-      <FloatingNavbar navItems={navItems}/>
+      <FloatingNavbar navItems={[{name:'Home',link:'/',icon:<FaHome/>}]}/>
       <Hero/>
       <Grid/>
       <RecentProjects/>
